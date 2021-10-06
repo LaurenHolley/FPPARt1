61d2ef46ae9911b979b5cc899bddb56d6ff909de
LaurenHolley <77747367+LaurenHolley@users.noreply.github.com>
2021-10-06 15:13:37 -0500

    Add files via upload

---
+++ project_part1.py
@@ -0,0 +1,92 @@
+import flask
+from flask import jsonify
+from flask import request, make_response
+from jinja2.loaders import ChoiceLoader, ModuleLoader
+from sql import create_connection
+from sql import execute_query
+from sql import execute_read_query
+
+#setting up an application name
+app = flask.Flask(__name__) #sets up the application
+app.config["DEBUG"] = True #allows errors to be shown in broswer
+
+#CRUD for User_Profile Table
+#Creating connection to the user profile table in the AWS database
+@app.route('/api-fp1/user_profile',methods=['GET'])
+def api_users():
+    conn = create_connection("cis3368.cemoodnlbqm2.us-east-2.rds.amazonaws.com", "admin","fall21CIS#", "cis3368fall21")
+    sql = "SELECT * FROM user_profile"
+    user_profile = execute_read_query(conn, sql)
+    results = []
+    for user in user_profile:
+        results.append(user)
+    return jsonify(results)
+
+#new route for adding user to user profile table using POST method
+@app.route('/api-fp1/add_user',methods=['POST'])
+def api_add_user():
+    conn = create_connection("cis3368.cemoodnlbqm2.us-east-2.rds.amazonaws.com", "admin","fall21CIS#", "cis3368fall21")
+    request_data = request.get_json()
+    new_fname = request_data['first_name']
+    new_lname = request_data['last_name']
+    sql = "INSERT INTO user_profile (first_name, last_name) VALUES ('%s','%s')" % (new_fname, new_lname)
+    execute_read_query(conn, sql)
+    return 'POST successful'
+
+#new route for updating user in user profile table using PUT method
+#@app.route('/api-fp1/update_user',methods=['PUT'])
+#def api_update_user():
+    #request_data = request.get_json()
+    #new_user_id = request_data['id']
+   # new_fname = request_data['first_name']
+    #new_lname = request_data['last_name']
+    #user_profile.append({'id':new_user_id,'first_name':new_fname,'last_name':new_lname})
+   # return 'PUT successful'
+
+#new route for deleting user from user profile table using DELETE method
+#@app.route('/api-fp1/delete_user',methods=['DELETE'])
+#def api_delete_user():
+    #request_data = request.get_json()
+    #new_user_id = request_data['id']
+    #new_fname = request_data['first_name']
+    #new_lname = request_data['last_name']
+    #user_profile.append({'id':new_user_id,'first_name':new_fname,'last_name':new_lname})
+    #return 'POST successful'
+
+#CRUD for Restaurant Tables
+#new route for adding a restaurant using POST method
+##@app.route('/api-fp1/add_restaurant',methods=['POST'])
+#def api_add_restaurant():
+    #request_data = request.get_json()
+    #newid = request_data['id']
+    #newmake = request_data['make']
+    #newmodel = request_data['model']
+   # newyear = request_data['year']
+    #newcolor = request_data['color']
+    #restaurant.append({'id':newid,'make':newmake,'model':newmodel,'year':newyear,'color':newcolor})
+    #return 'POST successful'
+
+#new route for updating restaurant in restaurant table using PUT method
+#@app.route('/api-fp1/update_restaurant',methods=['PUT'])
+#def api_update_restaurant():
+    #request_data = request.get_json()
+    #new_user_id = request_data['id']
+    #new_fname = request_data['first_name']
+    #new_lname = request_data['last_name']
+    #restaurant.append({'id':new_user_id,'first_name':new_fname,'last_name':new_lname})
+    #return 'POST successful'
+
+#new route for deleting restaurant from restaurant table using DELETE method
+#@app.route('/api-fp1/delete_restaurant',methods=['DELETE'])
+#def api_delete_restaurant():
+    #request_data = request.get_json()
+    #new_user_id = request_data['id']
+    #new_fname = request_data['first_name']
+    #new_lname = request_data['last_name']
+    #restaurant.append({'id':new_user_id,'first_name':new_fname,'last_name':new_lname})
+    #return 'POST successful'
+
+#Random Restaurant Selection
+
+app.run()
+
