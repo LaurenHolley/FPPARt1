50b4d0805e5d5f528da58aabee90bf4a024dbcd5
LaurenHolley <77747367+LaurenHolley@users.noreply.github.com>
2021-11-17 12:49:56 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 // load the things we need
 var express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 var app = express();
+
+var selectedID = "";
+app.use(bodyParser.urlencoded()); 
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+//required module to make calls to REST API
+const axios = require('axios');
 
 // use res.render to load up an ejs view file
 
@@ -12,14 +20,39 @@ app.get('/home', function(req, res) {
     res.render('pages/home');   
 });
 
-// edit user and restaurant profile page
+//dont forget to turn on the .py file for the api points
+
+app.post('/process_form', function(req, res){
+    //variable to hold response for users first name
+    first_name1 = req.body.first_name1
+    //variable to hold response for users last name
+    var last_name = req.body.last_name1
+    console.log("New user's first name is:"+first_name1);
+    var u_first_name= req.body.first_name2
+    console.log("Modified user's first name is:"+u_first_name);
+    // use res.render to load up an ejs view file
+    res.render('pages/editpgs', {body: req.body});
+    });
+
+
+// edit user and restaurants page
 app.get('/editpgs', function(req, res) {
-    res.render('pages/about');
+    //endpoint for adding a user
+    res.render('pages/editpgs')
 });
 
 // random restaurant selection page
 app.get('/randrest', function(req, res) {
-    res.render('pages/randrest');
+    axios.get('http://127.0.0.1:5000/api-fp1/user_profile')
+    .then((response)=>{
+        
+        var users = response.data;
+        console.log(users);
+        res.render('pages/randrest', {
+            users: users
+        });
+    });
+    
 });
 
 app.listen(8080);
