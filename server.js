42507dc1676f974c968ef77397db03c5f6f25d16
LaurenHolley <77747367+LaurenHolley@users.noreply.github.com>
2021-11-24 12:18:40 -0600

    Update server.js
    Added all the rendering of the new edit pages

--- server.js
+++ server.js
@@ -22,24 +22,89 @@ app.get('/home', function(req, res) {
 
 //dont forget to turn on the .py file for the api points
 
-app.post('/process_form', function(req, res){
+app.get('/add_user_edit', function(req, res) {
+    res.render('pages/add_user_edit');   
+});
+//this form is associated with adding a new user, it will take the input from the user and 
+//call the add_user API to update the database 
+app.post('/add_user_form', function(req, res){
     //variable to hold response for users first name
     first_name1 = req.body.first_name1
     //variable to hold response for users last name
-    var last_name = req.body.last_name1
-    console.log("New user's first name is:"+first_name1);
-    var u_first_name= req.body.first_name2
-    console.log("Modified user's first name is:"+u_first_name);
+    last_name1 = req.body.last_name1
     // use res.render to load up an ejs view file
-    res.render('pages/editpgs', {body: req.body});
+    res.render('pages/add_user_edit.ejs', {body: req.body});
     });
 
 
-// edit user and restaurants page
-app.get('/editpgs', function(req, res) {
-    //endpoint for adding a user
-    res.render('pages/editpgs')
+app.get('/update_user_edit', function(req, res) {
+    res.render('pages/update_user_edit');   
 });
+//this form is associated with updating a user profile, it will take the input from the user and 
+//call the update_user API to update the database 
+app.post('/update_user_form', function(req, res){
+    //variable to hold response for the id of user to update name
+    id2 = req.body.id2
+    //variable to hold response for users first name
+    first_name2 = req.body.first_name2
+    //variable to hold response for users last name
+    last_name2 = req.body.last_name2
+    // use res.render to load up an ejs view file
+    res.render('pages/edit_complete.ejs', {body: req.body});
+    });
+
+app.get('/delete_user_edit', function(req, res) {
+    res.render('pages/delete_user_edit');   
+});
+//this form is associated with deleting a user, it will take the input from the user and 
+//call the delete_user API to update the database 
+app.post('/delete_user_form', function(req, res){
+    //variable to hold response for the id of user to delete
+    id3 = req.body.id3
+    // use res.render to load up an ejs view file
+    res.render('pages/edit_complete.ejs', {body: req.body});
+    }); 
+
+app.get('/add_restaurant_edit', function(req, res) {
+    res.render('pages/add_restaurant_edit');   
+});
+//this form is associated with adding a new restaurant, it will take the input from the user and 
+//call the add_restaurant API to update the database 
+app.post('/add_restaurant_form', function(req, res){
+    //variable to hold response for the id of the user who wants to add a restaurant
+    id4 = req.body.id4
+    //variable to hold response for users first name
+    restuarant_name1 = req.body.restaurant_name1
+    // use res.render to load up an ejs view file
+    res.render('pages/edit_complete.ejs', {body: req.body});
+    });
+
+app.get('/update_restaurant_edit', function(req, res) {
+    res.render('pages/update_restaurant_edit');   
+});
+//this form is associated with adding updating a restaurant name, it will take the input from the user and 
+//call the update_restaurant API to update the database 
+app.post('/update_restaurant_form', function(req, res){
+    //variable to hold response for the id of the user who wants to add a restaurant
+    id5 = req.body.id5
+    //variable to hold response for users first name
+    restuarant_name2 = req.body.restaurant_name2
+    // use res.render to load up an ejs view file
+    res.render('pages/edit_complete.ejs', {body: req.body});
+    });
+ 
+app.get('/delete_restaurant_edit', function(req, res) {
+    res.render('pages/delete_restaurant_edit');   
+});
+//this form is associated with deleting a restaurant, it will take the restaurant ID from the user and 
+//call the delete_restaurant API to update the database 
+app.post('/delete_restaurant_form', function(req, res){
+    //variable to hold response for users first name
+    restuarant_name3 = req.body.restaurant_name3
+    // use res.render to load up an ejs view file
+    res.render('pages/edit_complete.ejs', {body: req.body});
+    });       
+
 
 // random restaurant selection page
 app.get('/randrest', function(req, res) {
